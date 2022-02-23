0d183395e20a0c934127398d3f2d0d5f7e4ee94b
iamzaidakbar <iamzaidakbar@gmail.com>
2022-02-23 12:16:01 +0530

    Changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,59 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'password-generator';
+
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  length = 0;
+  password = ''
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeLetters() {
+    this.includeLetters = !this.includeLetters
+  }
+
+  onChangeNumbers() {
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  onChangeSymbols() {
+    this.includeLetters = !this.includeSymbols
+  }
+  
+  onButtonCLick() {
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const numbers = '0123456789'
+    const symbols = '!@#$%^&*()'
+    
+    let wrapper = ''
+     if(this.includeLetters){
+        wrapper += letters
+     }
+     if(this.includeNumbers){
+       wrapper += numbers
+     }
+     if(this.includeSymbols){
+       wrapper += this.includeSymbols
+     }
+
+     let generatedNumbers = ''
+    for(let i=0;i < this.length;i++){
+       const randomNumbers = Math.floor(Math.random() * wrapper.length)
+       generatedNumbers += wrapper[randomNumbers]
+    }
+    this.password = generatedNumbers
+  }
+
+
 }
